8a2ca1f9469f912d4361a23a61644b2d1e60e6a2
Alp Ceylan <alp.ceylan07@gmail.com>
2020-11-20 12:01:41 +0300

    pit scouting detail screen commit

--- lib/screens/pit_scouting_detail_screen.dart
+++ lib/screens/pit_scouting_detail_screen.dart
@@ -19,19 +19,33 @@ class PitScoutingDetailScreen extends StatefulWidget {
 class _PitScoutingDetailScreenState extends State<PitScoutingDetailScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  int _chassisTypeInt;
   bool _climbing;
   bool _imageProcessing;
   int _imageProcessingTypeInt;
   int _shooterTypeInt;
+  bool _intake;
+  int _hoodTypeInt;
+  int _intakeTypeInt;
+  int _funnelTypeInt;
+  bool _autonomous;
+  int _chassisTypeInt;
 
-  int _newChassisTypeInt;
   bool _newClimbing;
   bool _newImageProcessing;
   int _newImageProcessingTypeInt;
   int _newShooterTypeInt;
+  bool _newIntake;
+  int _newHoodTypeInt;
+  int _newIntakeTypeInt;
+  int _newFunnelTypeInt;
+  bool _newAutonomous;
+  int _newChassisTypeInt;
 
+  int _maxBalls;
   String _climbingComment;
+  String _autonomousComment;
+  String _extra;
+  String _comment;
 
   @override
   Widget build(BuildContext context) {
@@ -41,17 +55,8 @@ class _PitScoutingDetailScreenState extends State<PitScoutingDetailScreen> {
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceWidth = MediaQuery.of(context).size.width;
 
-    if (_chassisTypeInt == null) {
-      if (team.chassisType == Chassis.X) {
-        _chassisTypeInt = 1;
-      } else if (team.chassisType == Chassis.Y) {
-        _chassisTypeInt = 2;
-      } else {
-        _chassisTypeInt = 3;
-      }
-    }
-
     if (_climbing == null) _climbing = team.climbing;
+    if (_maxBalls == null) _maxBalls = team.maxBalls;
     if (_imageProcessing == null) _imageProcessing = team.imageProcessing;
 
     if (_shooterTypeInt == null) {
@@ -64,14 +69,77 @@ class _PitScoutingDetailScreenState extends State<PitScoutingDetailScreen> {
       }
     }
 
-    if (_newChassisTypeInt == null) _newChassisTypeInt = _chassisTypeInt;
+    if (_imageProcessingTypeInt == null)
+      _imageProcessingTypeInt =
+          team.imageProcessingType == ImageProcessing.Custom ? 1 : 2;
+
+    if (_intake == null) _intake = team.intake;
+
+    if (_hoodTypeInt == null) {
+      if (team.hoodType == Hood.X) {
+        _hoodTypeInt = 1;
+      } else if (team.hoodType == Hood.Y) {
+        _hoodTypeInt = 2;
+      } else {
+        _hoodTypeInt = 3;
+      }
+    }
+
+    if (_intakeTypeInt == null) {
+      if (team.intakeType == Intake.X) {
+        _intakeTypeInt = 1;
+      } else if (team.intakeType == Intake.Y) {
+        _intakeTypeInt = 2;
+      } else {
+        _intakeTypeInt = 3;
+      }
+    }
+
+    if (_funnelTypeInt == null) {
+      if (team.funnelType == Funnel.X) {
+        _funnelTypeInt = 1;
+      } else if (team.funnelType == Funnel.Y) {
+        _funnelTypeInt = 2;
+      } else {
+        _funnelTypeInt = 3;
+      }
+    }
+
+    if (_autonomous == null) _autonomous = team.autonomous;
+
+    if (_chassisTypeInt == null) {
+      if (team.chassisType == Chassis.X) {
+        _chassisTypeInt = 1;
+      } else if (team.chassisType == Chassis.Y) {
+        _chassisTypeInt = 2;
+      } else {
+        _chassisTypeInt = 3;
+      }
+    }
+
     if (_newClimbing == null) _newClimbing = _climbing;
     if (_newImageProcessing == null) _newImageProcessing = _imageProcessing;
     if (_newShooterTypeInt == null) _newShooterTypeInt = _shooterTypeInt;
+    if (_newImageProcessingTypeInt == null)
+      _newImageProcessingTypeInt = _imageProcessingTypeInt;
+    if (_newIntake == null) _newIntake = _intake;
+    if (_newHoodTypeInt == null) _newHoodTypeInt = _hoodTypeInt;
+    if (_newFunnelTypeInt == null) _newFunnelTypeInt = _funnelTypeInt;
+    if (_newIntakeTypeInt == null) _newIntakeTypeInt = _intakeTypeInt;
+    if (_newAutonomous == null) _newAutonomous = _autonomous;
+    if (_newChassisTypeInt == null) _newChassisTypeInt = _chassisTypeInt;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(team.teamName),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              print("save button clicked.");
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -133,34 +201,10 @@ class _PitScoutingDetailScreenState extends State<PitScoutingDetailScreen> {
                         ),
                       ],
                     ),
-                    Container(
-                      width: deviceWidth * 0.3,
-                      height: deviceHeight * 0.065,
-                      child: DropdownButton(
-                        isExpanded: true,
-                        style: TextStyle(
-                          fontSize: 17,
-                          color: Colors.black,
-                        ),
-                        value: _newChassisTypeInt,
-                        items: [
-                          DropdownMenuItem(
-                            child: Text("Chassis X"),
-                            value: 1,
-                          ),
-                          DropdownMenuItem(
-                            child: Text("Chassis Y"),
-                            value: 2,
-                          ),
-                          DropdownMenuItem(
-                            child: Text("Chassis Z"),
-                            value: 3,
-                          ),
-                        ],
-                        onChanged: (value) {
-                          _newChassisTypeInt = value;
-                        },
-                      ),
+                    CustomTextInput(
+                      deviceWidth: deviceWidth,
+                      labelText: "Max balls",
+                      initialValue: "${team.maxBalls}",
                     ),
                   ],
                 ),
@@ -243,6 +287,261 @@ class _PitScoutingDetailScreenState extends State<PitScoutingDetailScreen> {
                       ),
                     ),
                   ],
+                ),
+                AnimatedContainer(
+                  height: _newImageProcessing ? deviceHeight * 0.05 : 0,
+                  duration: Duration(
+                    milliseconds: 300,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Image Processing Type:"),
+                      Container(
+                        width: deviceWidth * 0.3,
+                        height: deviceHeight * 0.065,
+                        child: DropdownButton(
+                          isExpanded: true,
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.black,
+                          ),
+                          value: _newImageProcessingTypeInt,
+                          items: [
+                            DropdownMenuItem(
+                              child: Text("Custom"),
+                              value: 1,
+                            ),
+                            DropdownMenuItem(
+                              child: Text("Limelight"),
+                              value: 2,
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _newImageProcessingTypeInt = value;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("Intake:"),
+                        Switch(
+                          value: _newIntake,
+                          activeTrackColor: Colors.lightBlueAccent,
+                          activeColor: Colors.blue,
+                          onChanged: (value) {
+                            setState(() {
+                              _newIntake = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: deviceWidth * 0.3,
+                      height: deviceHeight * 0.065,
+                      child: DropdownButton(
+                        isExpanded: true,
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.black,
+                        ),
+                        value: _newHoodTypeInt,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text("Hood X"),
+                            value: 1,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("Hood Y"),
+                            value: 2,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("Hood Z"),
+                            value: 3,
+                          ),
+                        ],
+                        onChanged: (value) {
+                          _newHoodTypeInt = value;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                AnimatedContainer(
+                  height: _newIntake ? deviceHeight * 0.05 : 0,
+                  duration: Duration(
+                    milliseconds: 300,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Intake Type:"),
+                      Container(
+                        width: deviceWidth * 0.3,
+                        height: deviceHeight * 0.065,
+                        child: DropdownButton(
+                          isExpanded: true,
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.black,
+                          ),
+                          value: _newIntakeTypeInt,
+                          items: [
+                            DropdownMenuItem(
+                              child: Text("Intake X"),
+                              value: 1,
+                            ),
+                            DropdownMenuItem(
+                              child: Text("Intake Y"),
+                              value: 2,
+                            ),
+                            DropdownMenuItem(
+                              child: Text("Intake Z"),
+                              value: 3,
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _newIntakeTypeInt = value;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("Autonomous::"),
+                        Switch(
+                          value: _newAutonomous,
+                          activeTrackColor: Colors.lightBlueAccent,
+                          activeColor: Colors.blue,
+                          onChanged: (value) {
+                            setState(() {
+                              _newAutonomous = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: deviceWidth * 0.3,
+                      height: deviceHeight * 0.065,
+                      child: DropdownButton(
+                        isExpanded: true,
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.black,
+                        ),
+                        value: _newChassisTypeInt,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text("Chassis X"),
+                            value: 1,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("Chassis Y"),
+                            value: 2,
+                          ),
+                          DropdownMenuItem(
+                            child: Text("Chassis Z"),
+                            value: 3,
+                          ),
+                        ],
+                        onChanged: (value) {
+                          _newChassisTypeInt = value;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                AnimatedContainer(
+                  height: _newAutonomous ? deviceHeight * 0.1 : 0,
+                  duration: Duration(
+                    milliseconds: 300,
+                  ),
+                  child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 2,
+                    initialValue: team.autonomousComment,
+                    decoration: InputDecoration(
+                      labelText: "Autonomous comment",
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (_newClimbing) {
+                        if (value.length == 0) {
+                          return "Autonomous comment shouldn't be empty.";
+                        }
+                        return null;
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _autonomousComment = newValue;
+                    },
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  initialValue: team.extra,
+                  decoration: InputDecoration(
+                    labelText: "Extra",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.length == 0) {
+                      return "Extra shouldn't be empty.";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    _extra = newValue;
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  initialValue: team.comment,
+                  decoration: InputDecoration(
+                    labelText: "Comment",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.length == 0) {
+                      return "Comment shouldn't be empty.";
+                    }
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    _comment = newValue;
+                  },
                 ),
               ],
             ),
